17fa46501fbbebb7703e009df53e26ecdb8387d9
Luis Ascencio Oberg <oberg.04101992@gmail.com>
2025-09-21 14:15:19 -0300

    revert: quitar HashRouter (app sin rutas)

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./App.css";
 
-const root = document.getElementById("root");
-if (!root) throw new Error("No se encontró el elemento #root en index.html");
-createRoot(root).render(
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
